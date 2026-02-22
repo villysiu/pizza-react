d981eb8d45f04607754aa4e3b550e75a2839f53d
Villy Siu <villysiu@gmail.com>
2026-02-22 13:49:33 -0800

    converted login and signup to madal

--- src/components/user/UserIcon.jsx
+++ src/components/user/UserIcon.jsx
@@ -1,42 +1,51 @@
 import { PersonCircle } from 'react-bootstrap-icons'
-import { Nav, NavDropdown, Spinner } from "react-bootstrap"
+import { Nav, NavDropdown, Navbar, Spinner } from "react-bootstrap"
 import { useNavigate } from 'react-router-dom'
 import { Link } from "react-router-dom"
 import {useAuth} from '../context/AuthContext'
+import AuthModal from '../auth/AuthModal'
 
 const UserIcon = () => {
     const navigate = useNavigate();
-    const { user,logout, loading } = useAuth();
-    const handleLogout = () => {
+    const { user, logout, loading, setShow, show } = useAuth();
+
+    const handleLogout = (e) => {
+        e.preventDefault();
         logout();
-        navigate("/login");
-};
-    if(loading){
-        return <Spinner animation="border" />;
+      
+    };
+    const handleClick = () => {
+        console.log("clicked", show)
+        setShow('login');
     }
+
     if(!user){
         return (
-            <Nav.Link as={Link} to="/login">
+            <>
+            <AuthModal />
+            <Nav.Link onClick={handleClick}>
                 <PersonCircle size={20} className="me-2" />
             </Nav.Link>
+            </>
         )
     }
     return (
         <Nav className="ms-auto">
             <NavDropdown 
+                as="div"
                 title={<PersonCircle size={20} />}
                 id="user-dropdown"
                 align='end'
                 
             >
                 <NavDropdown.Item as={Link} to="/orders">
-                    orders
+                    Orders
                 </NavDropdown.Item>
                 <NavDropdown.Item as={Link} to="/profile">
                     Profile
                 </NavDropdown.Item>
                 <NavDropdown.Divider />
-                <NavDropdown.Item as={Link} to="/logout" onClick={handleLogout}>
+                <NavDropdown.Item as="button" style={{cursor: 'pointer'}} onClick={handleLogout}>
                     Logout
                 </NavDropdown.Item>
             </NavDropdown>
