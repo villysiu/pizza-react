b2f40a7d812f46f9aeaa49cfac56383157a22d74
Villy Siu <villysiu@gmail.com>
2026-02-28 07:20:23 -0800

    number of item in cart

--- src/components/cart/CartIcon.jsx
+++ src/components/cart/CartIcon.jsx
@@ -1,4 +1,4 @@
-import { Cart } from 'react-bootstrap-icons'
+import { CartFill } from 'react-bootstrap-icons'
 import { Nav, Offcanvas, Spinner } from "react-bootstrap"
 import { Link } from "react-router-dom"
 import {useState} from 'react'
@@ -7,13 +7,14 @@ import { useCart } from '../context/CartContext'
 
 const CartIcon = () => {
     
-    const { show, setShow, loading } = useCart();
+    const { show, setShow, loading, carts } = useCart();
 
+    const count = carts.reduce((total, cart) => total + cart.quantity, 0 )
     if(!show && loading){
         return (
             <Nav.Link>
                 <Spinner animation="border"
-                            variant="danger"
+                            variant="secondary"
                             style={{ width: "20px", height: "20px" }}
                 />
             </Nav.Link >
@@ -21,17 +22,30 @@ const CartIcon = () => {
     }
 
     return (
-        <>
+        <div style={{ position: 'relative'}}>
        
-        <Nav.Link onClick={()=>setShow(true)} style={{cursor: 'pointer'}}>
-            <Cart size={20} className="me-2 cart-icon" />
-        </Nav.Link>
+            <Nav.Link onClick={()=>setShow(true)} style={{cursor: 'pointer'}}>
+                <CartFill size={22} className="me-2 cart-icon" />
+            </Nav.Link>
 
-        <CartOffcanvas />
-        
+            <CartOffcanvas />
+            {count > 0 && 
+                <div style={{ 
+                    position: 'absolute', 
+                    right: '-4px', 
+                    top: 0,
+                    border: 'solid 1px',
+                    width: '1.5rem', height: '1.5rem',
+                    borderRadius: '50%',
+                    backgroundColor: 'white',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    fontSize: '0.8rem',
+                }}>{count}</div>}
     
 
-        </>
+        </div>
     )
 }
 export default CartIcon
